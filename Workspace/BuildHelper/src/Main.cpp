01366015435770201e23bc7d53f855855c2ab054
EmpyreanArcadius <immaculateempyrean@gmail.com>
2020-06-09 10:57:09 +0530

    Added Functionality To Detect Memory Leaks.. (Memory Leaks Detected As Of This Version..)

--- Workspace/BuildHelper/src/Main.cpp
+++ Workspace/BuildHelper/src/Main.cpp
@@ -1,9 +1,34 @@
 #include<iostream>
 
+//This New Is Used Only For Debugging Purposes Set The Debugging Flag For The Event System To Use This...
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+
 #include "Symbols.h"
 #include "Example1.h"
 
 int main()
 {
+	//Start Initialize Memory Leak Checking Module..
+	int flag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG);
+	flag |= (_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetDbgFlag(flag);
+	//Ended Initialize Memory Leak Checking Module..
+
+
+	//Start Call Which Example You Want Here//
 	Example1Main();
+	//Ended Call Which Example You Want Here//
+	
+	int* n = new int[100];
+
+	//Start Print Detected Memory Leaks To File..
+	FILE* pFile;
+	freopen_s(&pFile, "MemoryLeakLog.txt", "w", stdout);
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDOUT);
+	_CrtDumpMemoryLeaks(); fclose(pFile);
+	//Ended Print Detected Memory Leaks To File..
+	
 }
