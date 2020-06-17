80c45acdf39a5bd59d8809656d9fc1acfb808c69
EmpyreanArcadius <immaculateempyrean@gmail.com>
2020-06-17 16:04:46 +0530

    Fixed Premake5 to not include the dynamic library version as it is not currently supported.

--- Workspace/BuildHelper/src/Main.cpp
+++ Workspace/BuildHelper/src/Main.cpp
@@ -22,9 +22,11 @@ int main()
 	//Start Call Which Example You Want Here//
 	Example1Main();
 	//Ended Call Which Example You Want Here//
+		
 	
 	ErmineEventSystem::EventBroadcastStation::DestroyStation(); //:> Forgot To Destroy The Station After Use.. Previously
-		
+	
+
 	//Start Print Detected Memory Leaks To File..
 	FILE* pFile;
 	freopen_s(&pFile, "MemoryLeakLog.txt", "w", stdout);
--- Workspace/premake5.lua
+++ Workspace/premake5.lua
@@ -6,7 +6,7 @@ workspace "EventSystem"
 	{
 		"Debug",
         "StaticLibRelease",
-        "DynamicLibRelease"
+        --"DynamicLibRelease" This Library Does Not Export Any Symbols Hence No lib will be generated .. please configure that dilema before attempting to genrate a dynamic library
 	}
 	
 	flags
@@ -19,7 +19,54 @@ workspace "EventSystem"
 
 --Start Defining The Main Project
 
-include "EventSystem"
+project "EventSystem"
+    location "EventSystem"
+
+	language "C++"
+    cppdialect "C++17"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+	{
+		"%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src"
+	}
+
+    defines
+	{
+        "_CRT_SECURE_NO_WARNINGS"
+	}
+	
+    filter "configurations:Debug"
+		kind "StaticLib"
+		runtime "Debug"
+		symbols "on"
+		staticruntime "on"
+
+    filter "configurations:StaticLibRelease"
+		kind "StaticLib"
+		runtime "Release"
+		optimize "on"
+		staticruntime "on"
+
+    --[[filter "configurations:DynamicLibRelease"
+		kind "SharedLib"
+		runtime "Release"
+		optimize "on"
+		staticruntime "off"
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/BuildHelper/\"")
+		   } --]] ---Enable This bit of code after fixing the problems with dynamic release library..
+
+
+	filter ""
 
 --BuildHelper Project
 
@@ -55,6 +102,7 @@ project "BuildHelper"
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
+		staticruntime "on"
 		defines
 		{
 			"ER_DEBUG_DEVELOP"
@@ -63,10 +111,12 @@ project "BuildHelper"
 	filter "configurations:StaticLibRelease"
 		runtime "Release"
 		optimize "on"
+		staticruntime "on"
 
-	filter "configurations:DynamicLibRelease"
-		runtime "Release"
-		optimize "on"
+	--[[ filter "configurations:DynamicLibRelease"
+		 runtime "Release"
+		 optimize "on"
+		 staticruntime "off" --]] --Enable This bit of code after fixing the problems with dynamic release library..
 
 	filter ""
 
